e5e698b4f6c317a169b5762c8bdc8d7629df53cc
Marc Strapetz <marc.strapetz@syntevo.com>
2019-10-17 23:23:11 +0200

    refactoring DirCacheTreeTest: more compact tests
    Introduce some utility methods to write tests more compact and
    make them better readable.
    
    Change-Id: I8dfe4d9df77bf95ad093be0e8fd2e7fdbb9ecce7
    Signed-off-by: Marc Strapetz <marc.strapetz@syntevo.com>

--- org.eclipse.jgit.test/tst/org/eclipse/jgit/dircache/DirCacheTreeTest.java
+++ org.eclipse.jgit.test/tst/org/eclipse/jgit/dircache/DirCacheTreeTest.java
@@ -44,7 +44,6 @@
 package org.eclipse.jgit.dircache;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertNull;
@@ -67,37 +66,28 @@ public class DirCacheTreeTest extends RepositoryTestCase {
 	@Test
 	public void testEmptyCache_CreateEmptyCacheTree() throws Exception {
 		final DirCache dc = db.readDirCache();
-		final DirCacheTree tree = dc.getCacheTree(true);
-		assertNotNull(tree);
-		assertSame(tree, dc.getCacheTree(false));
-		assertSame(tree, dc.getCacheTree(true));
-		assertEquals("", tree.getNameString());
-		assertEquals("", tree.getPathString());
-		assertEquals(0, tree.getChildCount());
-		assertEquals(0, tree.getEntrySpan());
-		assertFalse(tree.isValid());
+		final DirCacheTree tree = assertCacheTree(dc, true);
+		assertCacheTree(dc, true);
+		assertSame(tree, assertCacheTree(dc, false));
+		assertSame(tree, assertCacheTree(dc, true));
+		assertData(tree, "", "", 0, 0, false);
 	}
 
 	@Test
 	public void testEmptyCache_Clear_NoCacheTree() throws Exception {
 		final DirCache dc = db.readDirCache();
-		final DirCacheTree tree = dc.getCacheTree(true);
-		assertNotNull(tree);
+		final DirCacheTree tree = assertCacheTree(dc, true);
 		dc.clear();
 		assertNull(dc.getCacheTree(false));
-		assertNotSame(tree, dc.getCacheTree(true));
+		assertNotSame(tree, assertCacheTree(dc, true));
 	}
 
 	@Test
 	public void testSingleSubtree() throws Exception {
 		final DirCache dc = db.readDirCache();
 
-		final String[] paths = { "a-", "a/b", "a/c", "a/d", "a0b" };
-		final DirCacheEntry[] ents = new DirCacheEntry[paths.length];
-		for (int i = 0; i < paths.length; i++) {
-			ents[i] = new DirCacheEntry(paths[i]);
-			ents[i].setFileMode(FileMode.REGULAR_FILE);
-		}
+		final DirCacheEntry[] ents = createDirCacheEntries(
+				new String[] { "a-", "a/b", "a/c", "a/d", "a0b" });
 		final int aFirst = 1;
 		final int aLast = 3;
 
@@ -108,35 +98,22 @@ public class DirCacheTreeTest extends RepositoryTestCase {
 		b.finish();
 
 		assertNull(dc.getCacheTree(false));
-		final DirCacheTree root = dc.getCacheTree(true);
-		assertNotNull(root);
-		assertSame(root, dc.getCacheTree(true));
-		assertEquals("", root.getNameString());
-		assertEquals("", root.getPathString());
-		assertEquals(1, root.getChildCount());
-		assertEquals(dc.getEntryCount(), root.getEntrySpan());
-		assertFalse(root.isValid());
+		final DirCacheTree root = assertCacheTree(dc, true);
+		assertSame(root, assertCacheTree(dc, true));
+		assertData(root, "", "", 1, dc.getEntryCount(), false);
 
 		final DirCacheTree aTree = root.getChild(0);
 		assertNotNull(aTree);
 		assertSame(aTree, root.getChild(0));
-		assertEquals("a", aTree.getNameString());
-		assertEquals("a/", aTree.getPathString());
-		assertEquals(0, aTree.getChildCount());
-		assertEquals(aLast - aFirst + 1, aTree.getEntrySpan());
-		assertFalse(aTree.isValid());
+		assertData(aTree, "a", "a/", 0, aLast - aFirst + 1, false);
 	}
 
 	@Test
 	public void testTwoLevelSubtree() throws Exception {
 		final DirCache dc = db.readDirCache();
 
-		final String[] paths = { "a-", "a/b", "a/c/e", "a/c/f", "a/d", "a0b" };
-		final DirCacheEntry[] ents = new DirCacheEntry[paths.length];
-		for (int i = 0; i < paths.length; i++) {
-			ents[i] = new DirCacheEntry(paths[i]);
-			ents[i].setFileMode(FileMode.REGULAR_FILE);
-		}
+		final DirCacheEntry[] ents = createDirCacheEntries(
+				new String[] { "a-", "a/b", "a/c/e", "a/c/f", "a/d", "a0b" });
 		final int aFirst = 1;
 		final int aLast = 4;
 		final int acFirst = 2;
@@ -149,32 +126,19 @@ public class DirCacheTreeTest extends RepositoryTestCase {
 		b.finish();
 
 		assertNull(dc.getCacheTree(false));
-		final DirCacheTree root = dc.getCacheTree(true);
-		assertNotNull(root);
-		assertSame(root, dc.getCacheTree(true));
-		assertEquals("", root.getNameString());
-		assertEquals("", root.getPathString());
-		assertEquals(1, root.getChildCount());
-		assertEquals(dc.getEntryCount(), root.getEntrySpan());
-		assertFalse(root.isValid());
+		final DirCacheTree root = assertCacheTree(dc, true);
+		assertSame(root, assertCacheTree(dc, true));
+		assertData(root, "", "", 1, dc.getEntryCount(), false);
 
 		final DirCacheTree aTree = root.getChild(0);
 		assertNotNull(aTree);
 		assertSame(aTree, root.getChild(0));
-		assertEquals("a", aTree.getNameString());
-		assertEquals("a/", aTree.getPathString());
-		assertEquals(1, aTree.getChildCount());
-		assertEquals(aLast - aFirst + 1, aTree.getEntrySpan());
-		assertFalse(aTree.isValid());
+		assertData(aTree, "a", "a/", 1, aLast - aFirst + 1, false);
 
 		final DirCacheTree acTree = aTree.getChild(0);
 		assertNotNull(acTree);
 		assertSame(acTree, aTree.getChild(0));
-		assertEquals("c", acTree.getNameString());
-		assertEquals("a/c/", acTree.getPathString());
-		assertEquals(0, acTree.getChildCount());
-		assertEquals(acLast - acFirst + 1, acTree.getEntrySpan());
-		assertFalse(acTree.isValid());
+		assertData(acTree, "c", "a/c/", 0, acLast - acFirst + 1, false);
 	}
 
 	/**
@@ -192,13 +156,9 @@ public class DirCacheTreeTest extends RepositoryTestCase {
 
 		final String A = String.format("a%2000s", "a");
 		final String B = String.format("b%2000s", "b");
-		final String[] paths = { A + "-", A + "-" + B, A + "/" + B, A + "0" + B };
-		final DirCacheEntry[] ents = new DirCacheEntry[paths.length];
-		for (int i = 0; i < paths.length; i++) {
-			ents[i] = new DirCacheEntry(paths[i]);
-			ents[i].setFileMode(FileMode.REGULAR_FILE);
-		}
-
+		final String[] paths = { A + "-", A + "-" + B, A + "/" + B,
+				A + "0" + B };
+		final DirCacheEntry[] ents = createDirCacheEntries(paths);
 		final DirCacheBuilder b = dc.builder();
 		for (DirCacheEntry ent : ents) {
 			b.add(ent);
@@ -208,6 +168,30 @@ public class DirCacheTreeTest extends RepositoryTestCase {
 		DirCache read = db.readDirCache();
 
 		assertEquals(paths.length, read.getEntryCount());
-		assertEquals(1, read.getCacheTree(true).getChildCount());
+		assertEquals(1, assertCacheTree(dc, true).getChildCount());
+	}
+
+	private DirCacheTree assertCacheTree(DirCache cache, boolean build) {
+		final DirCacheTree tree = cache.getCacheTree(build);
+		assertNotNull(tree);
+		return tree;
+	}
+
+	private DirCacheEntry[] createDirCacheEntries(String[] paths) {
+		final DirCacheEntry[] ents = new DirCacheEntry[paths.length];
+		for (int i = 0; i < paths.length; i++) {
+			ents[i] = new DirCacheEntry(paths[i]);
+			ents[i].setFileMode(FileMode.REGULAR_FILE);
+		}
+		return ents;
+	}
+
+	private void assertData(DirCacheTree tree, String name, String path,
+			int childCount, int entrySpan, boolean valid) {
+		assertEquals(name, tree.getNameString());
+		assertEquals(path, tree.getPathString());
+		assertEquals(childCount, tree.getChildCount());
+		assertEquals(entrySpan, tree.getEntrySpan());
+		assertEquals(valid, tree.isValid());
 	}
 }
